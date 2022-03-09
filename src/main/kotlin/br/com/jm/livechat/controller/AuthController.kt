7369b21091e152d4eff7cc68a69b1af538c567b0
Jonathan Andrade <85408369+jmaxsa@users.noreply.github.com>
2022-03-09 19:29:53 -0300

    Removing unnused files

--- src/main/kotlin/br/com/jm/livechat/controller/AuthController.kt
+++ src/main/kotlin/br/com/jm/livechat/controller/AuthController.kt
@@ -5,7 +5,12 @@ import br.com.jm.livechat.models.UserRegisterBody
 import br.com.jm.livechat.repositories.UserRepository
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.*
+import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.PathVariable
 import java.util.Optional
 
 @RestController
